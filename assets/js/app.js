94681760ae9edd28f1d3955cc4d904b1aab90d1a
AllanDonato7 <allandonato.dev@gmail.com>
2024-11-06 03:28:01 -0300

    Alteração nos projetos

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+/*--=========== LOADER ============- */
+document.addEventListener('DOMContentLoaded', () => {
+    let tl = new TimelineMax();
+
+    tl.fromTo('.bg-loader', 1,
+    {width: '100%'},
+    {width: '0%', delay: 5, ease: Expo.easeInOut})
+})
